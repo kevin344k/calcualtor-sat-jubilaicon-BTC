ff59d5cd309d95ba8a2480af5bda04080c2b3258
kevin344k <kevin344k@gamil.com>
2023-11-17 05:15:38 +0000

    set index.html

--- index.js
+++ index.js
@@ -2,14 +2,10 @@ const express =require("express")
 const app=express()
 
 app.set("port", process.env.PORT || 4000);
+app.use(express.static('public'))
 
 app.get('/',(req,res)=>{  //cuando el clinte colocite acceso a la ruta primcipal el servidor va a escuchar con get
-    res.sendFile('./public/index.html',{
-        root:__dirname
-    }) //luego de escuchar la petición va ha enviar el archivo como respuesta 
-   {
-
-   }
+    res.sendFile(index.html) //luego de escuchar la petición va ha enviar el archivo como respuesta 
 })
 
 app.listen(app.get("port"),()=>{
