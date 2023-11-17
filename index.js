6fdb01969042cbb6d24e660b899da9b1bcd00337
kevin344k <kevin344k@gamil.com>
2023-11-17 05:05:40 +0000

    set index.html

--- index.js
+++ index.js
@@ -4,12 +4,14 @@ const port=3000 || process.env.PORT
 
 
 
-app.use(express.static("public"))
+app.get('/',(req,res)=>{  //cuando el clinte colocite acceso a la ruta primcipal el servidor va a escuchar con get
+    res.sendFile('./public/index.html',{
+        root:__dirname
+    }) //luego de escuchar la petición va ha enviar el archivo como respuesta 
+   {
 
-app.get("/",(req,res)=>{
-  res.sendFile(index.html)
+   }
 })
-
 
 app.listen(port,()=>{
   console.log("server is running on port",port)
