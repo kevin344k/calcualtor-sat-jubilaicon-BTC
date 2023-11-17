a0327f20e7ba082156f5615080f6f801d7fb6c33
kevin344k <kevin344k@gamil.com>
2023-11-17 04:40:09 +0000

    set server

---
+++ index.js
@@ -0,0 +1,17 @@
+const express =require("express")
+const app=express()
+const port=3000 || process.env.PORT
+
+
+
+app.use(express.static("public"))
+
+app.get("/",(req,res)=>{
+  res.send("index.html")
+})
+
+
+app.listen(port,()=>{
+  console.log("server is running on port",port)
+
+})
